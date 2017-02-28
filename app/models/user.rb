4bc864cf7d9b04a24d010c2ab8057c6be6ab1ded
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-02-27 19:16:58 -0800

    Create static pages controller, basic header, home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence: true, length: { maximum: 255 },
-  #                   format: { with: VALID_EMAIL_REGEX },
-  #                   uniqueness: { case_sensitive: false }
 end
