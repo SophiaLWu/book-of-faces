3698ee9e0b92fe8057ea5fdde6fe2f88b6fae791
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-03-02 14:12:40 -0800

    Add post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,21 @@ users = User.create([{ name: "Snoopy", email: "snoopy@peanuts.com",
 										 { name: "Peppermint Patty", email: "patty@peanuts.com", 
 											 password: "peanuts" },
 										 { name: "Violet Gray", email: "violet@peanuts.com", 
-											 password: "peanuts" },
-											 ])
+											 password: "peanuts" }])
+
+# Post seeds
+posts = Post.create([{ content: "The joy is in the playing.", user_id: 6 },
+										 { content: "I got a rock.", user_id: 2 },
+										 { content: "Happiness is a warm puppy", user_id: 5 },
+										 { content: "It was a dark and stormy night...", 
+										 	 user_id: 1 },
+										 { content: "I was a victim of false doctrine.", 
+										 	 user_id: 3 },
+										 { content: "Stop calling me sir!", user_id: 9 },
+										 { content: "That's the way it goes...", user_id: 2 },
+										 { content: "Kiss her, you blockhead!", user_id: 7 },
+										 { content: "There are three things I have learned never "\
+										 	          "to discuss with people...religion, politics" \
+										 	          "and the Great Pumpkin!", user_id: 3 }])
+
+
