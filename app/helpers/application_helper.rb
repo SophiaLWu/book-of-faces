f58f8486d51374e7923b7f8ecbe82fb187f270b4
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-03-03 19:43:01 -0800

    Modify message to users that need to log in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def logged_in_user
     unless user_signed_in?
-      flash[:danger] = "Please log in."
+      flash[:danger] = "You need to sign in or sign up before continuing."
       redirect_to new_user_session_path
     end
   end
