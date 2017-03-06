d9924e1cb521ef7b5997132101bc5f0cbad78dc0
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-03-05 18:41:23 -0800

    Add functionality of editing and deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :logged_in_user
+	#before_action :correct_user, only: [:edit, :destroy]
 
 	def create
 		@comment = current_user.comments.build(comment_params)
@@ -11,9 +12,38 @@ class CommentsController < ApplicationController
 		redirect_back(fallback_location: root_path)
 	end
 
+	def edit
+		@comment = Comment.find(params[:id])
+		@post = @comment.post
+	end
+
+	def update
+		@comment = Comment.find(params[:id])
+		if @comment.update_attributes(comment_params)
+			flash[:success] = "Comment updated."
+			redirect_to root_path
+		else
+			flash[:danger] = "Comment not updated."
+			render "edit"
+		end
+	end
+
+	def destroy
+		Comment.find(params[:id]).destroy
+		flash[:success] = "Comment deleted."
+		redirect_back(fallback_location: root_path)
+	end
+
+
 	private
 
 		def comment_params
 			params.require(:comment).permit(:content, :post_id)
+		end
+
+		def correct_user
+			unless Comment.find(params[:id]).belongs_to?(current_user)
+				redirect_back(fallback_location: root_path)
+			end
 		end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
 	belongs_to :post, :inverse_of => :comments
 	belongs_to :user, :inverse_of => :comments
+
+  # Returns true if a comment belongs to the given user and false otherwise
+  def belongs_to?(user)
+    user_id == user.id
+  end
 end
