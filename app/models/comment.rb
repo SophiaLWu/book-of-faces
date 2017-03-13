e6e66de71eac2687fe435c7824bc79e5fd1f74a5
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-03-13 14:16:15 -0700

    Sort comments by date and include timestamps

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
 	belongs_to :post, :inverse_of => :comments
 	belongs_to :user, :inverse_of => :comments
 
+  default_scope -> { order(created_at: :asc) }
+
   # Returns true if a comment belongs to the given user and false otherwise
   def belongs_to?(user)
     user_id == user.id
