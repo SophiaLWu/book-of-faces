2d57709e4879528ccdcbc46aca5fc2a59eb81e70
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-06-03 10:16:14 -0700

    Add unit tests for Friendships model

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:snoopy)
+    @user2 = users(:charlie)
+    @friendship = @user.requested_friendships.build(friended_id: @user2.id)
+  end
+
+  test "should be valid" do
+    assert @friendship.valid?
+  end
+
+  test "friender id should be present" do
+    @friendship.friender_id = nil
+    assert_not @friendship.valid?
+  end
+
+  test "friended id should be present" do
+    @friendship.friended_id = nil
+    assert_not @friendship.valid?
+  end
+
+  test "friendship starts at not accepted" do
+    assert_equal(@friendship.accepted, false)
+  end
+
+  test "friendship can be accepted" do
+    assert_not @friendship.accepted
+    @friendship.accept_friend_request
+    assert @friendship.accepted
+  end
+
 end
