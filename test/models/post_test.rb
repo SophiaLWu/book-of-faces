2ab62726cc6da168623063b2c2b24d228559191c
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-06-02 19:15:44 -0700

    Add unit tests for Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,64 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:snoopy)
+    @post = @user.posts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.user_id = "    "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 500 characters" do
+    @post.content = "a" * 501
+    assert_not @post.valid?
+  end
+
+  test "picture does not have to be present" do
+    @post.picture = nil
+    assert @post.valid?
+  end
+
+  test "order should be most recent first" do
+    post2 = @user.posts.build(content: "Hello")
+    post3 = @user.posts.build(content: "Hello there")
+    @post.save
+    post2.save
+    post3.save
+    assert_equal post3, Post.first
+  end
+
+  test "associated comments should be destroyed" do
+    @user.save
+    post = @user.posts.build(content: "Lorem ipsum")
+    post.save
+    post.comments.create!(content: "Lorem ipsum galgusgn", 
+                          user_id: @user.id)
+    assert_difference 'Comment.count', -1 do
+      post.destroy
+    end
+  end
+
+  test "associated likes should be destroyed" do
+    @user.save
+    post = @user.posts.build(content: "Lorem ipsum")
+    post.save
+    post.likes.create!(user_id: @user.id)
+    assert_difference 'Like.count', -1 do
+      post.destroy
+    end
+  end
+
 end
