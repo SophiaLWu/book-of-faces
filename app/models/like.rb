8a2ccbf309f0a329563dc25feb5b2977255c1ba0
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-06-02 19:28:30 -0700

    Add unit tests for Likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,8 @@
 class Like < ApplicationRecord
 	belongs_to :post, :inverse_of => :likes
 	belongs_to :user, :inverse_of => :likes
-	validates :user_id, uniqueness: {scope: :post_id}
+
+  default_scope -> { order(created_at: :desc) }
+	validates :user_id, presence: true, uniqueness: {scope: :post_id}
+  validates :post_id, presence: true
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:snoopy)
+    @post = @user.posts.build(content: "Lorem ipsum")
+    @post.save
+    @like = @post.likes.build(user_id: @user.id)
+    @user2 = users(:charlie)
+    @post2 = @user2.posts.build(content: "Lorem ipsum 2")
+    @post2.save
+    @like2 = @post2.likes.build(user_id: @user2.id)
+  end
+
+  test "should be valid" do
+    assert @like.valid?
+  end
+
+  test "user id should be present" do
+    @like.user_id = nil
+    assert_not @like.valid?
+  end
+
+  test "post id should be present" do
+    @like.post_id = nil
+    assert_not @like.valid?
+  end
+
+  test "order should be most recent first" do
+    @like.save
+    @like2.save
+    assert_equal @like2, Like.first
+  end
+  
+  test "can't like the same post twice" do
+    assert_no_difference 'Comment.count' do
+      like3 = @post.likes.build(user_id: @user.id)
+      like3.save
+    end
+  end
 end
